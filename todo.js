d205e9d0b8fa884fc6fe69c0c1f3ebd220e17248
Saha-Jewel <jewelsaha072@gmail.com>
2026-02-23 14:42:51 +0900

    feat: add task creation and delete with event delegation

---
+++ todo.js
@@ -0,0 +1,44 @@
+const btn = document.getElementById("btn");
+const inputText = document.querySelector("input[type='text']")
+const unordered = document.getElementById("unordered")
+
+
+btn.addEventListener("click", (e)=>{
+    e.preventDefault()
+   const textValue = inputText.value;
+   const date = new Date()
+   const li = document.createElement("li")
+   if(textValue===""){
+    alert("please input the text")
+    return
+   }
+//   onclick="deleteTask(this)
+ li.innerHTML= `
+ <span>${textValue}</span>
+ <span>${date}</span>
+ <button>Edit</button>
+ <button">Delete</button>
+ `
+ unordered.appendChild(li)
+ li.style.color="red"
+
+ inputText.value=""
+})
+
+// event delegation
+unordered.addEventListener("click", (e)=>{
+const clickedElement = e.target;
+const createInput = document.createElement("input")
+if(clickedElement.innerText==="Delete"){
+    clickedElement.parentElement.remove()
+}
+else if(clickedElement.innerText==="Edit"){
+   alert(inputText.value)
+}
+})
+
+
+// onclick listener
+// function deleteTask(button){
+// button.parentElement.remove()
+// }
